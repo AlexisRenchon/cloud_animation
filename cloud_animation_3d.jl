e4c845f5b1981ffb1ae2bf02d34e8c1f33183e9a
Alexandre A. Renchon <a.renchon@gmail.com>
2025-09-17 13:16:25 -0700

    better earth image

--- cloud_animation_3d.jl
+++ cloud_animation_3d.jl
@@ -1,6 +1,9 @@
 using GLMakie
 using GeoMakie
 using ClimaAnalysis
+using FileIO
+
+earth_img = load(download("https://upload.wikimedia.org/wikipedia/commons/5/56/Blue_Marble_Next_Generation_%2B_topography_%2B_bathymetry.jpg"))
 
 fig = Figure(size = (2000, 2000), backgroundcolor = :black)
 ax = GeoMakie.GlobeAxis(fig[1,1]; show_axis = false)
@@ -12,8 +15,10 @@ lat = var.dims["lat"]
 
 surface!(ax,
          -180..180, -90..90,
-         zeros(axes(rotr90(GeoMakie.earth())));
-         shading = NoShading, color = rotr90(GeoMakie.earth())
+         zeros(axes(rotr90(earth_img)));
+         shading = NoShading,
+         color = rotr90(earth_img),
+         backlight = 1.5f0,
         )
 
 # lines!(ax, GeoMakie.coastlines(), color = :black)
@@ -25,11 +30,15 @@ var_data = @lift($var_time.data)
 
 limits = (0, 0.04)
 
+# var_data_normalized = @lift(($var_data .- minimum($var_data)) ./ (maximum($var_data) - minimum($var_data)))
+
 p = surface!(ax, lon, lat, var_data,
              zlevel = 300_000,
              colorrange = limits,
              colormap = Reverse(:PuBu),
+             #colormap = [:white, :white],
              shading = NoShading,
+             #             alpha = var_data_normalized,
              alpha = 0.3,
              transparency = true,
              highclip = :white,
@@ -44,3 +53,12 @@ record(fig, "animation.mp4", 1:total_frames; framerate = 20, compression = 10) d
     time[] = t
     rotate_cam!(ax.scene, 0, -0.015, 0)
 end
+
+#
+#record(fig, "animation.gif", 1:total_frames; framerate = 20, compression = 40) do t
+#    if t == 1
+#        zoom!(ax.scene, 0.6)
+#    end
+#    time[] = t
+#    rotate_cam!(ax.scene, 0, -0.015, 0)
+#end
